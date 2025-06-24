6401daabc9dd334e529f456bd96a6be4bf1ff28e
Lucas <birdex.104@gmail.com>
2025-06-24 19:59:56 -0300

    first commit

---
+++ src/main/java/com/birdex/BirdexApplication.java
@@ -0,0 +1,13 @@
+package com.birdex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birdex/BirdexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
