3c0926d14fa3beeb7692422440bf2a8e5410b749
Nicol <nicolas.mannarino@hotmail.com>
2025-09-06 20:22:18 -0300

    Agrego lo de Fede y además se logro que funcione por el tema del error del tiempo

--- src/main/java/com/birdex/BirdexApplication.java
+++ src/main/java/com/birdex/BirdexApplication.java
@@ -1,13 +1,32 @@
 package com.birdex;
 
+import java.util.TimeZone;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BirdexApplication {
 
+	@Bean
+	ApplicationRunner debugProps(Environment env) {
+	return args -> {
+		System.out.println(">> JDBC URL = " + env.getProperty("spring.datasource.url"));
+		System.out.println(">> JVM TZ   = " + TimeZone.getDefault().getID());
+	};
+	}
+
+	static {
+    TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
+    System.setProperty("user.timezone", "America/Argentina/Buenos_Aires");
+  	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BirdexApplication.class, args);
 	}
 
 }
+
