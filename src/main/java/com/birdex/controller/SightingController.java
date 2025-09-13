6aae41e08cbef77a8d7719cebb4820c05f641875
fefortuna-sp <federico.fortuna@seedpath.io>
2025-09-12 22:01:29 -0300

    implement endpoint to get sightings per userEmail ordered by dateTime

--- src/main/java/com/birdex/controller/SightingController.java
+++ src/main/java/com/birdex/controller/SightingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/sightning")
+@RequestMapping("/sighting")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class SightingController {
