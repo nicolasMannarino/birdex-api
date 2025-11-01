ab55828c01aaf554d68a9aeeabfa903f603f79ee
fefortuna-sp <federico.fortuna@seedpath.io>
2025-11-01 15:27:33 -0300

    add missions & achievements to new user

--- src/main/java/com/birdex/service/RegisterService.java
+++ src/main/java/com/birdex/service/RegisterService.java
@@ -2,12 +2,14 @@ package com.birdex.service;
 
 
 import com.birdex.domain.RegisterRequest;
-import com.birdex.entity.UserEntity;
-import com.birdex.repository.UserRepository;
+import com.birdex.entity.*;
+import com.birdex.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +17,10 @@ public class RegisterService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MissionRepository missionRepository;
+    private final AchievementRepository achievementRepository;
+    private final UserMissionRepository userMissionRepository;
+    private final UserAchievementRepository userAchievementRepository;
 
     @Transactional
     public void register(RegisterRequest req) {
@@ -26,5 +32,29 @@ public class RegisterService {
                 .build();
 
         userRepository.save(user);
+
+        var missions = missionRepository.findAll();
+        for (MissionEntity m : missions) {
+            var um = UserMissionEntity.builder()
+                    .user(user)
+                    .mission(m)
+                    .progress(new HashMap<String, Object>())
+                    .completed(false)
+                    .claimed(false)
+                    .build();
+            userMissionRepository.save(um);
+        }
+
+        var achievements = achievementRepository.findAll();
+        for (AchievementEntity a : achievements) {
+            var ua = UserAchievementEntity.builder()
+                    .user(user)
+                    .achievement(a)
+                    .progress(new HashMap<String, Object>())
+                    .claimed(false)
+                    .obtainedAt(null)
+                    .build();
+            userAchievementRepository.save(ua);
+        }
     }
 }
